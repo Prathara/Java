413a5ecddb5839cb7ab96daacae5da2141456ec8
Prathara <prarthana070606@gmail.com>
2025-06-05 12:43:39 +0530

    Add files via upload
    Bio

---
+++ Hello.java
@@ -0,0 +1,14 @@
+public class Hello{
+    public static void main (String[] args){
+       String name = "Thara";
+       int age = 18;
+       double height = 5.1;
+       char grade = 'A';
+       boolean isStudent = true;
+       System.out.println("My name is " + name);
+       System.out.println("My age is " + age);
+       System.out.println("My height is" + height);
+       System.out.println("My grade is " + grade);
+       System.out.println("Am I a student " + isStudent);
+    }
+}
